738210f9b81067ff7b034cb3c952f0daa01857cd
Akshaya <akshayakrsh@gmail.com>
2015-11-13 13:24:54 +0700

    Fix build scripts on linux OS

--- hooks/hook-execute-bit-fix.js
+++ hooks/hook-execute-bit-fix.js
@@ -1,7 +1,7 @@
 /*
-  Copyright (c) Microsoft. All rights reserved.  
-  Licensed under the MIT license. See LICENSE file in the project root for full license information.
-*/
+ Copyright (c) Microsoft. All rights reserved.
+ Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
 var fs = require("fs");
 var path = require("path");
 var exec = require("child_process").exec;
@@ -9,11 +9,11 @@ var Q;
 
 module.exports = function(context) {
   // Only bother if we're on OSX and are after platform add for iOS itself (still need to run for other platforms)
-  if(process.platform =="darwin" && 
-      context.opts && 
-      context.opts.cordova && 
-      context.opts.cordova.platforms && 
-      !(context.hook == "before_platform_add" && context.opts.platforms.indexOf("ios") >= 0)) {
+  if(process.platform === "darwin" &&
+    context.opts &&
+    context.opts.cordova &&
+    context.opts.cordova.platforms &&
+    context.hook !== "before_platform_add") {
 
     // Grab the Q, glob node modules from cordova
     Q=context.requireCordovaModule("q");
@@ -21,11 +21,11 @@ module.exports = function(context) {
     var deferred = Q.defer();
 
     // Generate the script to set execute bits for installed platforms
-    var script ="";
+    var script = "";
     context.opts.cordova.platforms.forEach(function(platform) {
       script += "find -E platforms/" + platform + "/cordova -type f -regex \"[^.(LICENSE)]*\" -exec chmod +x {} +\n"
     });
-    
+
     // Run script
     exec(script, function(err, stderr, stdout) {
       if(err) deferred.reject(err);
@@ -33,7 +33,7 @@ module.exports = function(context) {
       if(stdout) console.log(stdout);
       deferred.resolve();
     });
-    
+
     return deferred.promise;
   }
 }
